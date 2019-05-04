e9d04076d60d26ef53acba65dc8002bcb764899b
Mike Duister <mike@duister.me>
2019-05-04 12:53:39 +0200

    Fixed indentation.

--- src/helpers/getStyles.ts
+++ src/helpers/getStyles.ts
@@ -6,9 +6,9 @@ export function getStyles(styles: Styles): FlattenSimpleInterpolation {
 
 	for (const styleKey of Object.keys(styles)) {
 		stylesInterpolation = css`
-				${stylesInterpolation};
-				${styleKey.substr(0, 2) === '--' ? styleKey : `--${styleKey}`}: ${styles[styleKey]};
-			`
+			${stylesInterpolation};
+			${styleKey.substr(0, 2) === '--' ? styleKey : `--${styleKey}`}: ${styles[styleKey]};
+		`
 	}
 
 	return stylesInterpolation
